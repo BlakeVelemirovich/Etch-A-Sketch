a2f48885746cc97f0fe5c2602f034754af904713
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-03-09 12:30:28 -0400

    Created grid, footer, and header

--- Etch-A-Sketch.js
+++ Etch-A-Sketch.js
@@ -1,10 +1,16 @@
 const gridContainer = document.querySelector('.gridContainer');
 let sixteenBySixteen = 256;
 
+function gridColour {
+
+}
+
 for (let i = 0; i < sixteenBySixteen ; i++){
 	const gridPixel = document.createElement('div');
 	gridPixel.classList.add('gridPixel');
 	gridContainer.appendChild(gridPixel);
 }
 
-
+gridPixel.addEventListener('mouseenter', function() {
+	gridColour;
+});
