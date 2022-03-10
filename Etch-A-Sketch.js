7e475e506047a4d468171ab80d651949d3bd68e2
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-03-10 13:33:27 -0400

    Reset button added

--- Etch-A-Sketch.js
+++ Etch-A-Sketch.js
@@ -6,7 +6,7 @@ function gridColour(gridPixelColour) {
 const gridContainer = document.querySelector('.gridContainer');
 let sixteenBySixteen = 256;
 
-for (let i = 0; i < sixteenBySixteen ; i++){
+for (let i = 0; i < sixteenBySixteen; i++){
 	const gridPixel = document.createElement('div');
 	gridPixel.classList.add('gridPixel');
 	gridContainer.appendChild(gridPixel);
@@ -22,3 +22,11 @@ gridContainer.addEventListener('mouseover', function (e) {
 		gridColour(pixel);
 	}
 });
+
+// Reset button
+const reset = document.querySelector('.resetBtn');
+
+reset.addEventListener('click', function () {
+	window.location.reload();
+});
+
